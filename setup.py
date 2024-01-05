385e51a431735e790665650412995449d90bb80a
Charles Ferguson <gerph@gerph.org>
2024-01-05 23:39:14 +0000

    Add a very simple setup.py to package up for use by others.
    The setup.py provides a package that should let us upload it to
    PyPI at some point. The name of the tool I've selected is following
    the scheme of `riscos-<risc os command name>`, and as I've just
    created `*DumpI` as an companion to `*Dump` in the same way that
    `*MemoryI` is a companion to `*Memory`, this works.

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+"""
+Packaging for the riscos_disassemble package.
+"""
+
+from distutils.core import setup
+import setuptools  # noqa
+
+from os import path
+# io.open is needed for projects that support Python 2.7
+# It ensures open() defaults to text mode with universal newlines,
+# and accepts an argument to specify the text encoding
+# Python 3 only projects can skip this import
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name = 'riscos-disassemble',
+    packages = ['riscos_disassemble'],
+    version = '0.1.0',
+    license='MIT',
+    description = 'Disassemble ARM and Thumb files like RISC OS',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    author = 'Charles Ferguson',
+    author_email = 'gerph@gerph.org',
+    url = 'https://github.com/gerph/riscos-disassemble-python',
+    keywords = ['disassemble', 'arm', 'riscos'],
+    entry_points={ 'console_scripts': ['riscos-dumpi = riscos_disassemble.__main__:main'] },
+    install_requires= [
+        ],
+    classifiers= [
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            "Operating System :: OS Independent",
+        ],
+    python_requires='>=2.7',
+)
